aa3ce7b211f21777d8bf120c332d4b123191f8f6
Sharmistha Parui <paruisharmistha72@gmail.com>
2021-01-24 20:06:40 +0530

    extended SpringBootServletInitializer

--- movie-cruiser/src/main/java/com/cognizant/moviecruiser/MovieCruiserApplication.java
+++ movie-cruiser/src/main/java/com/cognizant/moviecruiser/MovieCruiserApplication.java
@@ -2,9 +2,10 @@ package com.cognizant.moviecruiser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MovieCruiserApplication {
+public class MovieCruiserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		
